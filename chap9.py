1719ba8a78187deed372745f2a0e958931506736
Sudan Shahi <Sudan.shahi@metropolia.fi>
2026-03-13 23:15:18 +0200

    Add files via upload

---
+++ chap9.py
@@ -0,0 +1,84 @@
+class MachineNetwork:
+    def __init__(self):
+        self.machine_links = {}
+
+    def add_machine(self, machine):
+        if machine not in self.machine_links:
+            self.machine_links[machine] = []
+
+    def add_link(self, m1, m2):
+        if m1 not in self.machine_links:
+            self.add_machine(m1)
+        if m2 not in self.machine_links:
+            self.add_machine(m2)
+
+        if m2 not in self.machine_links[m1]:
+            self.machine_links[m1].append(m2)
+        if m1 not in self.machine_links[m2]:
+            self.machine_links[m2].append(m1)
+
+    def print_network(self):
+        for machine, connections in self.machine_links.items():
+            print(f"{machine}: {connections}")
+
+    def print_connected_machines(self, machine):
+        if machine in self.machine_links:
+            print(f"{machine} is connected to: {self.machine_links[machine]}")
+        else:
+            print(f"{machine} not found in network")
+
+    def bfs(self, start):
+        if start not in self.machine_links:
+            print(f"Error: {start} does not exist")
+            return []
+
+        visited = []
+        queue = [start]
+
+        while queue:
+            machine = queue.pop(0)
+            if machine not in visited:
+                visited.append(machine)
+                for neighbor in self.machine_links[machine]:
+                    if neighbor not in visited and neighbor not in queue:
+                        queue.append(neighbor)
+
+        return visited  # Make sure this is here!
+
+    def dfs(self, start):
+        if start not in self.machine_links:
+            print(f"Error: {start} does not exist")
+            return []
+
+        visited = []
+        stack = [start]
+
+        while stack:
+            machine = stack.pop()
+            if machine not in visited:
+                visited.append(machine)
+                for neighbor in self.machine_links[machine]:
+                    if neighbor not in visited:
+                        stack.append(neighbor)
+
+        return visited  # Make sure this is here!
+
+
+# Create the network
+network = MachineNetwork()
+
+# Add links as specified
+network.add_link("Machine_A", "Machine_B")
+network.add_link("Machine_A", "Machine_C")
+network.add_link("Machine_B", "Machine_D")
+network.add_link("Machine_C", "Machine_D")
+network.add_link("Machine_C", "Machine_E")
+
+# Print the network first
+print("Machine Network:")
+network.print_network()
+print()
+
+# Test BFS and DFS
+print("BFS from Machine_A:", network.bfs("Machine_A"))
+print("DFS from Machine_A:", network.dfs("Machine_A"))
